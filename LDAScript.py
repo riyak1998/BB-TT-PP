f4f1cb18f8ed0d0b0cc26b28041f1ac7e24c1fd9
Dhruva Bhagdikar <dhruva@Dhruvas-MacBook-Pro.local>
2019-11-25 15:59:28 +0530

    result.txt added

--- LDAScript.py
+++ LDAScript.py
@@ -1,6 +1,7 @@
 import nltk
 import logging
 import numpy as np
+import string
 from gensim import corpora
 from gensim import models
 from gensim.test.utils import datapath
@@ -9,13 +10,19 @@ from six import iteritems
 from csvTOsql import fetchall,connect
 from sklearn.metrics.pairwise import cosine_similarity
 import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 def process(test_string):
-    bad = ['`','~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','[','}','}','|',':',';','"','<',',','>','.','?','/','0','1','2','3','4','5','6','7','8','9']
-    for i in bad: 
-        test_string = test_string.replace(i, ' ') 
-    return test_string
+    # bad = ['`','~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','[','}','}','|',':',';','"','<',',','>','.','?','/','0','1','2','3','4','5','6','7','8','9']
+    good = set(string.ascii_lowercase).union(set(string.ascii_uppercase).union(set("'")))
+    new_string = ""
+    for index in range(0,len(test_string)):
+        if test_string[index] not in good:
+            new_string+=" "
+        else:
+            new_string+=test_string[index]
+    t = len(new_string)
+    return new_string[0:t-12]
 
 class MyCorpus:
     __instance = None
@@ -30,7 +37,7 @@ class MyCorpus:
         self.query = "SELECT content FROM articles"
         self.cursor.execute(self.query)
         self.row = self.cursor.fetchone()
-        self.name = "final_dictionary_1"
+        self.name = "final_dictionary_2"
         try:
             self.dictionary = corpora.Dictionary().load(datapath(self.name))
         except:
@@ -43,7 +50,7 @@ class MyCorpus:
             stop_ids = [
                 self.dictionary.token2id[stopword]
                 for stopword in self.stoplist
-                if stopword in self.dictionary.token2id]
+                if stopword in self.dictionary.token2id ]
             once_ids = [tokenid for tokenid, docfreq in iteritems(self.dictionary.dfs) if docfreq == 1]
             self.dictionary.filter_tokens(stop_ids + once_ids)
             self.dictionary.compactify()
@@ -70,29 +77,29 @@ class Model:
             Model()
         return Model.__instance
     def __init__(self):
-        self.name = "final_model_1"
+        self.name = "final_model_3"
         self.dimensions = 50
-        self.iterations = 10
-        self.passes = 10
+        self.iterations = 400
+        self.passes = 50
         self.alhpa = 'auto'
         self.eta = 'auto'
+        self.eval_every = 1
         self.corpus = MyCorpus.getInstance()
         try:
             self.model = models.LdaModel.load(datapath(self.name))
         except:
-            self.model =  models.ldamulticore.LdaMulticore(self.corpus, id2word=self.corpus.dictionary, num_topics=self.dimensions, iterations=self.iterations, passes=self.passes,minimum_probability =0.0)
+            self.model =  models.ldamulticore.LdaMulticore(self.corpus, id2word=self.corpus.dictionary, eval_every = self.eval_every, num_topics=self.dimensions, iterations=self.iterations, passes=self.passes,minimum_probability =0.0)
             self.model.save(datapath(self.name))
         Model.__instance = self
     def getVector(self,article):
-        # print(datapath(self.name))
         return self.model[self.corpus.dictionary.doc2bow(process(article).lower().split())]
     def getReccommendation(self,vector):
         index=1
         similarities = []
-        b = np.array([i[1] for i in vector]).reshape(1,50)
+        b = np.array([i[1] for i in vector]).reshape(1,self.dimensions)
         for bow in self.corpus:
             bb = self.model[bow]
-            a = np.array([i[1] for i in bb ]).reshape(1,50)
+            a = np.array([i[1] for i in bb ]).reshape(1,self.dimensions)
             simi = cosine_similarity(a, b)
             similarities.append((simi[0][0],index))
             index+=1
@@ -100,7 +107,12 @@ class Model:
         l = [s[1] for s in similarities]
         return l
     def reTrain(self):
-        self.model =  models.ldamulticore.LdaMulticore(self.corpus, id2word=self.corpus.dictionary, num_topics=self.dimensions, iterations=self.iterations, passes=self.passes,minimum_probability =0.0)
+        self.model =  models.ldamulticore.LdaMulticore(self.corpus, id2word=self.corpus.dictionary, eval_every = self.eval_every, num_topics=self.dimensions, iterations=self.iterations, passes=self.passes,minimum_probability =0.0)
         self.model.save(datapath(self.name))
 mymodel = Model.getInstance()
-print(mymodel.model.show_topics(num_topics=50, num_words=5, log=False, formatted=False))
+# print(mymodel.model.show_topics(num_topics=50, num_words=5, log=False, formatted=False))
+# print(process("it-s"))
+# l = fetchall()
+# print(process(l[0]).split())
+# for s in stopwords:
+# cor = MyCorpus.getInstance()
